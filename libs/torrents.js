3ab9c2e1259d80f73742f29680a89f6c1be09152
joe <jchimien@mail.usf.edu>
2017-04-23 09:29:08 -0400

    save info by id

--- libs/torrents.js
+++ libs/torrents.js
@@ -4,7 +4,7 @@ const webtorrent = require('webtorrent'),
     Promise = require('bluebird');
 
 const exampleTorrent = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent';
-const filePath = path.join(process.cwd() + '/downloadingTorrents');
+const filePath = path.join(process.cwd(), 'downloadingTorrents');
 
 class torrentHandler {
     constructor(mongo) {
@@ -21,11 +21,11 @@ class torrentHandler {
             return this.torrentClient.add(url, {path}, torrent => {
                 torrent.on('done', () => {
 
-                    const {name} = torrent;
+                    const {name, id} = torrent;
 
-                    console.log('completed downloading torrent', name);
+                    console.log('completed downloading torrent', name, id);
 
-                    return this.saveTorrentInfo(name).then(() => resolve(torrent));
+                    return this.saveTorrentInfo(id).then(() => resolve(torrent));
 
                 });
 
@@ -36,9 +36,7 @@ class torrentHandler {
                     console.log(`progress: ${(torrent.progress * 100).toFixed(0)}%`)
                 });
 
-                torrent.on('error', err => {
-                    reject(err);
-                });
+                torrent.on('error', err => reject(err));
 
 
             });
@@ -46,10 +44,11 @@ class torrentHandler {
 
     }
 
-    saveTorrentInfo(name) {
-        return this.models.torrents.updateOne({name}, {
+    saveTorrentInfo(id) {
+        return this.models.torrents.updateOne({id}, {
             completed: true,
-            completedAt: moment().format('YYY-MM-DD hh:mm A'),
+            downloading: false,
+            completedAt: new Date(),
         });
     }
 
@@ -57,7 +56,7 @@ class torrentHandler {
         return Promise.filter(torrents, t => t.verified);
     }
 
-    sortByRatio(results) {
+    sortBySeeders(results) {
 
         return results.sort((a, b) => b.seeders - a.seeders);
 
